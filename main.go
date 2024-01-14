74fa1e1e86c016ae25db9cbfe95a72974a672a1d
renxzen <renxzen@gmail.com>
2024-01-14 18:45:08 -0500

    added tailwindcss with bun
    added tailwind
    
    lint

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
+	e.Static("/static", "cmd/static")
 
 	api.RegisterUi(e)
 
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./cmd/**/*.{html,templ}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
