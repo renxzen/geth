7c70be2c9b3febd49772d222452fd1888495008f
renxzen <renxzen@gmail.com>
2024-01-14 21:45:53 -0500

    sample global state added

--- cmd/todo/api/ui.go
+++ cmd/todo/api/ui.go
@@ -2,11 +2,16 @@ package api
 
 import (
 	"context"
+	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 
+	"github.com/renxzen/geth/cmd/todo/domain"
 	"github.com/renxzen/geth/cmd/todo/views"
 )
+
+var global domain.GlobalState
 
 func RegisterUi(e *echo.Echo) {
 	r := e.Group("/todo/ui")
@@ -22,7 +27,60 @@ func RegisterUi(e *echo.Echo) {
 	})
 
 	r.GET("/list", func(c echo.Context) error {
-		component := views.List()
+		component := views.List(global.Todos)
 		return component.Render(context.Background(), c.Response().Writer)
+	})
+
+	r.POST("", func(c echo.Context) error {
+		content := c.FormValue("content")
+		if content == "" {
+			return c.String(http.StatusBadRequest, "Content cannot be empty")
+		}
+
+		todo := domain.Todo{
+			Id:        int64(len(global.Todos) + 1),
+			Content:   content,
+			Completed: false,
+		}
+
+		global.Todos = append(global.Todos, todo)
+
+		component := views.Todo(todo)
+		return component.Render(context.Background(), c.Response().Writer)
+	})
+
+	r.PUT("/:id", func(c echo.Context) error {
+		paramId := c.Param("id")
+		id, err := strconv.Atoi(paramId)
+		if err != nil {
+			return c.String(http.StatusBadRequest, "Invalid id")
+		}
+
+		for idx := range global.Todos {
+			if global.Todos[idx].Id == int64(id) {
+				global.Todos[idx].Completed = !global.Todos[idx].Completed
+				component := views.Todo(global.Todos[idx])
+				return component.Render(context.Background(), c.Response().Writer)
+			}
+		}
+
+		return c.String(http.StatusBadRequest, "Todo not found")
+	})
+
+	r.DELETE("/:id", func(c echo.Context) error {
+		paramId := c.Param("id")
+		id, err := strconv.Atoi(paramId)
+		if err != nil {
+			return c.String(http.StatusBadRequest, "Invalid id")
+		}
+
+		for idx := range global.Todos {
+			if global.Todos[idx].Id == int64(id) {
+				global.Todos = append(global.Todos[:idx], global.Todos[idx+1:]...)
+				return c.NoContent(http.StatusNoContent)
+			}
+		}
+
+		return c.String(http.StatusBadRequest, "Todo not found")
 	})
 }
---
+++ cmd/todo/domain/model.go
@@ -0,0 +1,11 @@
+package domain
+
+type Todo struct {
+	Id        int64  `json:"id"`
+	Content   string `json:"content"`
+	Completed bool   `json:"completed"`
+}
+
+type GlobalState struct {
+	Todos []Todo
+}
